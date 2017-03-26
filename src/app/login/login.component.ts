8977cfeb749038d9dcd061aef921e5ff83b24bd4
Dmitry Shutov <7dmitry77@gmail.com>
2017-03-26 12:22:46 +0400

    Login component done

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {LoginService} from "../services/login.service";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [LoginService],
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent{
 
-  constructor() { }
+  constructor(private loginService: LoginService) { }
 
-  ngOnInit() {
+  tryToLogin(login: string, password: string) {
+    if (login && password) {
+      this.loginService.login(login, password)
+        .subscribe(
+          credentials => console.log(credentials),
+          error => console.log(error));
+    }
   }
 
 }
