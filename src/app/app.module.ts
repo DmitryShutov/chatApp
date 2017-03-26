2aacd9b0de2673558f5560684c84fa64d7fa50e4
Dmitry Shutov <7dmitry77@gmail.com>
2017-03-26 11:24:22 +0400

    Make login page style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {MaterialModule} from "@angular/material";
+import {RouterModule} from "@angular/router";
+import {LoginComponent} from './login/login.component';
+
+import { appRoutes } from './app.routes'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule,
+    RouterModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import {Route, Routes} from "@angular/router";
+import {LoginComponent} from "./login/login.component";
+
+export const appRoutes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent,
+  }
+];
