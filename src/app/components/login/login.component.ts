0f13231d23c9a616124878bc3be6f44b24d70dc4
Dmitry Shutov <7dmitry77@gmail.com>
2017-07-06 13:01:19 +0400

    fix styles after @angular/material updates

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -9,7 +9,7 @@ import {UserDataService} from '../../services/user-data.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
 
